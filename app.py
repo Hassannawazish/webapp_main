48d8cdf3b7cab19b8d5daafdfe92f4c4b1e62046
HassanNawazish <hassannawazishrasool1@gmail.com>
2022-03-15 12:52:57 +0500

    setting proctitle

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import setproctitle
+
 from application.init import app
 from flask import render_template
 @app.route('/')
@@ -5,4 +7,5 @@ def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
+    setproctitle.setproctitle('Hassans_website')
     app.run(debug=True)
